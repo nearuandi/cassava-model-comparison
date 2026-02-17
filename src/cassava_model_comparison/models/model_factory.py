74e825e7ca3210111d300e7e0e3fbf6061c8cb51
mff5 <166507311+mff5@users.noreply.github.com>
2026-02-17 12:54:11 +0900

    model_factory 단순화

--- src/cassava_model_comparison/models/model_factory.py
+++ src/cassava_model_comparison/models/model_factory.py
@@ -1,17 +1,9 @@
-from typing import Callable
-
 import torch.nn as nn
 
 from .simple_cnn import build_simple_cnn
 from .resnet18 import build_resnet18
 from .mobilenet_v2 import build_mobilenet_v2
 
-
-MODEL_REGISTRY: dict[str, Callable[..., nn.Module]] = {
-    "simple_cnn": build_simple_cnn,
-    "resnet18": build_resnet18,
-    "mobilenet_v2": build_mobilenet_v2,
-}
 
 def build_model(
     model_name: str,
@@ -22,13 +14,9 @@ def build_model(
 
     model_name = model_name.lower()
 
-    build_fn = MODEL_REGISTRY[model_name]
-
     if model_name == "simple_cnn":
-        return build_fn(num_classes=num_classes)
-
-    return build_fn(
-        num_classes=num_classes,
-        pretrained=pretrained,
-        freeze_backbone=freeze_backbone,
-    )
+        return build_simple_cnn(num_classes=num_classes)
+    if model_name == "mobilenet_v2":
+        return build_mobilenet_v2(num_classes=num_classes, pretrained=pretrained, freeze_backbone=freeze_backbone)
+    if model_name == "resnet18":
+        return build_resnet18(num_classes=num_classes, pretrained=pretrained, freeze_backbone=freeze_backbone)
